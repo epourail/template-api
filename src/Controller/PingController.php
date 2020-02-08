23aacce114d5da5d19b4053eba735f4af8faac3d
Julien Verger <julien.verger@orange.com>
2020-02-08 13:31:16 +0100

    wip

--- src/Controller/PingController.php
+++ src/Controller/PingController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,11 +15,23 @@ use Symfony\Component\HttpFoundation\Response;
  */
 final class PingController extends AbstractController
 {
+    private $logger;
+
+    /**
+     * PingController constructor.
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/ping", name="app_ping")
      */
     public function ping(): Response
     {
+        $this->logger->info(__METHOD__);
         return new Response(
             'pong',
             Response::HTTP_OK,
